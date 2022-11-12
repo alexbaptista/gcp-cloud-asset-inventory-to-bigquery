456fd32f1b397425cb0297cdc8392c4c99bfae6d
Alex Baptista <alexcaio5@gmail.com>
2022-11-12 17:05:30 -0300

    fix region

--- infra-as-code/terraform/google_bigquery_dataset.tf
+++ infra-as-code/terraform/google_bigquery_dataset.tf
@@ -1,5 +1,5 @@
 resource "google_bigquery_dataset" "cloud_asset_inventory" {
   dataset_id  = var.bigquery_dataset_cloud_asset_inventory.dataset_id
   description = var.bigquery_dataset_cloud_asset_inventory.description
-  location    = data.google_client_config.current
+  location    = data.google_client_config.current.region
 }
