68d54684fc113906f1c42ed44859245c34b336e9
Alex Baptista <alexcaio5@gmail.com>
2022-11-12 20:28:06 -0300

    update

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
 # gcp-cloud-asset-inventory-to-bigquery
 Proof of Concept for to get data from Cloud Asset Inventory and send to BigQuery
 
+# Requirements
 
+# Suggestion to execute Terraform files
 
+### Create a local file (*.tfvars) for backend/provider settings:
+
+```provider.tfvars```
+```
+project          = "dummy-project"
+region           = "us-central1"
+credentials_file = "/user/gcp/service_account_key.json"
+```
+
+```backend.tfvars```
+```
+bucket      = "dummy-bucket"
+prefix      = "terraform/state"
+credentials = "/user/gcp/service_account_key.json"
+```
+
+### Run Terraform with variable file
+
+```Init```
+```
+terraform init -backend-config=backend.tfvars
+```
+
+```Plan```
+```
 terraform plan -var-file=provider.tfvars 
+```
+
+```Apply```
+```
+terraform apply -var-file=provider.tfvars 
+```
--- infra-as-code/terraform/google_storage_bucket_object.tf
+++ infra-as-code/terraform/google_storage_bucket_object.tf
@@ -1,16 +1,18 @@
 resource "google_storage_bucket_object" "get_cloudsql_instance_inventory" {
-  bucket = google_storage_bucket.cloud_functions.name
-  name   = "cloudfunctions/${var.cloud_functions_get_cloudsql_instance_inventory_settings.name}.zip"
-  source = data.archive_file.get_cloudsql_instance_inventory.output_path
+  bucket         = google_storage_bucket.cloud_functions.name
+  name           = "cloudfunctions/${var.cloud_functions_get_cloudsql_instance_inventory_settings.name}.zip"
+  source         = data.archive_file.get_cloudsql_instance_inventory.output_path
+  detect_md5hash = data.archive_file.get_cloudsql_instance_inventory.output_md5
   depends_on = [
     google_storage_bucket.cloud_functions
   ]
 }
 
 resource "google_storage_bucket_object" "put_cloudsql_users_to_bigquery" {
-  bucket = google_storage_bucket.cloud_functions.name
-  name   = "cloudfunctions/${var.cloud_functions_put_cloudsql_users_to_bigquery_settings.name}.zip"
-  source = data.archive_file.put_cloudsql_users_to_bigquery.output_path
+  bucket         = google_storage_bucket.cloud_functions.name
+  name           = "cloudfunctions/${var.cloud_functions_put_cloudsql_users_to_bigquery_settings.name}.zip"
+  source         = data.archive_file.put_cloudsql_users_to_bigquery.output_path
+  detect_md5hash = data.archive_file.put_cloudsql_users_to_bigquery.output_md5
   depends_on = [
     google_storage_bucket.cloud_functions
   ]
